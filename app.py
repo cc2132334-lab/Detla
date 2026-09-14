d218af08cfc81494fd755f320c7d9ed2dffe8b9d
cc2132334-lab <cc2132334@gmail.com>
2026-09-15 01:27:56 +0530

    Change title from 'Delta Terminal' to 'DELTAEXCHANGE TERMINAL'

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ terminal_html = r"""
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-<title>Delta Terminal</title>
+<title>DELTAEXCHANGE TERMINAL</title>
 
 <style>
 :root[data-theme="dark"]{
